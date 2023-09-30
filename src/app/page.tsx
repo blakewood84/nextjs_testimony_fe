62e321c102babccd2ddd5916a319b0c746bad0a1
Blake Wood Jr <blakewoodjr84@gmail.com>
2023-09-30 11:31:52 -0700

    Trying to add dark mode support.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,7 @@
-"use client";
-import Image from "next/image";
-import { useEffect } from "react";
-
-// Check the user's preference, and load the user's preference
-// If the user changes the theme, add the theme to local storage
-//
-
 export default function Home() {
-  useEffect(() => {
-    const preference = window.matchMedia(
-      "(prefers-color-scheme: light)"
-    ).matches;
-    console.log(preference);
-  }, []);
-  return <main className=""></main>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>My Home Page</h1>
+    </main>
+  );
 }
